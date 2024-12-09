2e878cee0e3956bcbec13cea84c050e433b27017
Reza Rahem <101524351+rezarahem@users.noreply.github.com>
2024-12-09 11:10:25 +0330

    up

--- src/commands/main/create-base-docker-compose/create-docker-compose-base.ts
+++ src/commands/main/create-base-docker-compose/create-docker-compose-base.ts
@@ -2,7 +2,7 @@ import fs from 'fs';
 import path from 'path';
 import { createFile } from '../../../utils/create-file.js';
 
-export const createDockerComposeBase = async () => {
+export const createDockerComposeBase = async (app: string) => {
     const templatePath = path.resolve(__dirname, './docker-compose-base.template');
     const templateContent = fs.readFileSync(templatePath, 'utf8');
 
