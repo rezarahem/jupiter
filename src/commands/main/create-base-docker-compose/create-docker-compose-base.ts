85187857756b088b6456de1f8fd84c1ff4790764
Reza Rahem <101524351+rezarahem@users.noreply.github.com>
2024-12-09 10:59:10 +0330

    add base docker compose to main

---
+++ src/commands/main/create-base-docker-compose/create-docker-compose-base.ts
@@ -0,0 +1,10 @@
+import fs from 'fs';
+import path from 'path';
+import { createFile } from '../../../utils/create-file.js';
+
+export const createDockerComposeBase = async () => {
+    const templatePath = path.resolve(__dirname, './docker-compose-base.template');
+    const templateContent = fs.readFileSync(templatePath, 'utf8');
+
+    createFile('./docker-compose.base.yml', '.env', templateContent)
+}
