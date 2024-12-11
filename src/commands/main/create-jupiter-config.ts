5d4e0b9ddcf34471a697bfc96d4666760d9f1d71
rahem <reza.rahem224@gmail.com>
2024-12-11 19:27:01 +0330

    update create config

--- src/commands/main/create-jupiter-config.ts
+++ src/commands/main/create-jupiter-config.ts
@@ -1,20 +1,14 @@
 import { createFile } from '../../utils/create-file.js';
 
-
 type CreateConfigFileProps = {
   directory: string;
-  app: string;
   [key: string]: string | unknown;
 };
 export const createConfigFile = async ({
-  app,
   directory,
   ...configProps
 }: CreateConfigFileProps) => {
   const juConfig = {
-    dir: app,
-    image: app,
-    net: app,
     ...configProps,
   };
   const content = JSON.stringify(juConfig, null, 2);
