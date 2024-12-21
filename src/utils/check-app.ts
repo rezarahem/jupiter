a5111865226dfb5dbe73b5f94ca3ed61a8229142
rahem <reza.rahem224@gmail.com>
2024-12-21 15:04:18 +0330

    update console message for app support clarification

--- src/utils/check-app.ts
+++ src/utils/check-app.ts
@@ -4,7 +4,7 @@ export const checkApp = async () => {
   const app = await findApp();
 
   if (app !== "nextjs") {
-    // console.log('Jupiter currently only supports Next.js and Nuxt.js projects. Please create a Next.js or Nuxt.js app using the command: npx create-next-app or npx create-nuxt-app');
+    // console.log('Jupiter currently supports only Next.js and Nuxt.js projects. Please create a project using: npx create-next-app or npx create-nuxt-app.');
     console.log(
       "Jupiter currently only supports Next.js projects. Please create a Next.js app using the command: npx create-next-app"
     );
