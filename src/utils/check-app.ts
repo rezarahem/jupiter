9688aba00f31e8fdc566bcb6b4f684d726285e37
rahem <reza.rahem224@gmail.com>
2024-12-21 17:16:54 +0330

    refactor app check utility to read package.json directly and remove find-app module

--- src/utils/check-app.ts
+++ src/utils/check-app.ts
@@ -1,12 +1,25 @@
-import { findApp } from "./find-app.js";
+import { promises as fs } from 'fs';
+import * as path from 'path';
 
 export const checkApp = async () => {
-  const app = await findApp();
+  let app: string = '';
+  try {
+    const packageJsonPath = path.resolve(process.cwd(), 'package.json');
+    const packageJson = JSON.parse(await fs.readFile(packageJsonPath, 'utf-8'));
+    if (!packageJson || !packageJson.dependencies) return;
 
-  if (app !== "nextjs") {
+    if (packageJson.dependencies.next) {
+      app = 'nextjs';
+    } else if (packageJson.dependencies.nuxt) {
+      app = 'nuxtjs';
+    }
+  } catch (error) {}
+
+
+  if (app !== 'nextjs') {
     // console.log('Jupiter currently supports only Next.js and Nuxt.js projects. Please create a project using: npx create-next-app or npx create-nuxt-app.');
     console.log(
-      "Jupiter currently only supports Next.js projects. Please create a Next.js app using the command: npx create-next-app"
+      'Jupiter currently only supports Next.js projects. Please create a Next.js app using the command: npx create-next-app'
     );
     process.exit(1);
   }
