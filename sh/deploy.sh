2b990bc6f1d8d7505a404f9acf5a3f208a643e48
rahem <reza.rahem224@gmail.com>
2024-12-29 21:18:33 +0330

    add nginx conf for req limit

--- sh/deploy.sh
+++ sh/deploy.sh
@@ -14,27 +14,20 @@ if [[
   exit 1
 fi
 
-
-# Check if SSL certificate exists for the given domain
-if [ ! -f /etc/letsencrypt/live/$DOMAIN/fullchain.pem ]; then
-  ./jux/set-ssl.sh $DOMAIN $EMAIL
-  if [ $? -eq 0 ]; then
-    echo "SSL certificate successfully created for $DOMAIN"
-  else
-    echo "Error: Failed to create SSL certificate for $DOMAIN"
-    exit 1
-  fi
+# Define the path to the rate limit config file in conf.d
+rate_limit_config="/etc/nginx/conf.d/rate_limit.conf"
+# Check if the rate limit configuration file exists
+if [ ! -f "$rate_limit_config" ]; then
+    # If the file doesn't exist, create it
+    echo "Creating rate_limit.conf in /etc/nginx/conf.d/..."
+    echo "limit_req_zone \$binary_remote_addr zone=mylimit:10m rate=10r/s;" | sudo tee "$rate_limit_config" > /dev/null
+    echo "Rate limit configuration created at $rate_limit_config"
+    
+    # Reload nginx to apply the changes
+    echo "Reloading Nginx to apply changes..."
+    sudo systemctl reload nginx
 else
-  echo "SSL certificate already exists for $DOMAIN"
-fi
-
-# Verify SSL certificate
-openssl x509 -in /etc/letsencrypt/live/$DOMAIN/fullchain.pem -noout -text > /dev/null 2>&1
-if [ $? -eq 0 ]; then
-  echo "SSL certificate for $DOMAIN is valid"
-else
-  echo "Error: SSL certificate for $DOMAIN is invalid"
-  exit 1
+    echo "Rate limit configuration already exists at $rate_limit_config"
 fi
 
 # Check if reverse proxy configuration exists for the given domain
@@ -58,6 +51,30 @@ else
   echo "Error: Nginx configuration test failed"
   exit 1
 fi
+
+
+# Check if SSL certificate exists for the given domain
+if [ ! -f /etc/letsencrypt/live/$DOMAIN/fullchain.pem ]; then
+  ./jux/set-ssl.sh $DOMAIN $EMAIL
+  if [ $? -eq 0 ]; then
+    echo "SSL certificate successfully created for $DOMAIN"
+  else
+    echo "Error: Failed to create SSL certificate for $DOMAIN"
+    exit 1
+  fi
+else
+  echo "SSL certificate already exists for $DOMAIN"
+fi
+
+# Verify SSL certificate
+openssl x509 -in /etc/letsencrypt/live/$DOMAIN/fullchain.pem -noout -text > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+  echo "SSL certificate for $DOMAIN is valid"
+else
+  echo "Error: SSL certificate for $DOMAIN is invalid"
+  exit 1
+fi
+
 
 
 # Change directory to the project folder
