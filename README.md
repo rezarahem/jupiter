b62a4d914530e33b0659561b3522c6c6ced54b89
rahem <reza.rahem224@gmail.com>
2024-12-31 20:08:12 +0330

    update nextjs dockerfile

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Jupiter
 
-**Jupiter** is an in-progress CLI tool designed to simplify the process of deploying modern web applications.
+Jupiter is an in-progress CLI tool designed to simplify the process of deploying modern web applications.
--- src/commands/create-app/fn/add-next/docker.ts
+++ src/commands/create-app/fn/add-next/docker.ts
@@ -5,31 +5,33 @@ export const docker = async () => {
   dotenv.config({ path: '.jupiter' });
 
   const content = `#1
-    FROM node:22-alpine AS base 
-    WORKDIR /prod
-    RUN apk add --no-cache curl
-    
-    #2
-    FROM base AS build 
-    ENV NODE_ENV=build
-    COPY package.json package-lock.json ./
-    RUN npm ci --verbose 
-    COPY . .
-    RUN npm run build
-    
-    # Remove dev dependencies
-    RUN npm prune --production --verbose
-    
-    #3
-    FROM base AS prod
-    ENV NODE_ENV=production
-    COPY --from=build /prod/public ./public
-    COPY --from=build /prod/.next/standalone ./
-    COPY --from=build /prod/.next/static ./.next/static
-    
-    EXPOSE 3000
-    
-    CMD ["node", "server.js"]`;
+FROM node:22-alpine AS base 
+WORKDIR /prod
+RUN apk add --no-cache curl
+
+#2
+FROM base AS build 
+ENV NODE_ENV=build
+RUN npm config set registry https://registry.npmmirror.com
+COPY package.json package-lock.json ./
+RUN npm ci --verbose 
+COPY . .
+RUN npm run build
+
+# Remove dev dependencies
+RUN npm prune --production --verbose
+
+#3
+FROM base AS prod
+ENV NODE_ENV=production
+COPY --from=build /prod/public ./public
+COPY --from=build /prod/.next/standalone ./
+COPY --from=build /prod/.next/static ./.next/static
+
+EXPOSE 3000
+
+CMD ["node", "server.js"]
+`;
 
   await createFile({
     content,
