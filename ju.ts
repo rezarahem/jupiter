92275e519c503a77ae202758a3767b7efe003eb8
rahem <reza.rahem224@gmail.com>
2025-01-01 23:11:00 +0330

    add version flag

--- ju.ts
+++ ju.ts
@@ -3,14 +3,17 @@
 import { program } from 'commander';
 import { CreateApp } from './src/commands/create-app/create-app.js';
 import { Deploy } from './src/commands/deploy/deploy.js';
+import { UpdateScript } from './src/commands/update-scripts/update-scripts.js';
 
 program
   .name('Jupiter')
   .description(
     'Jupiter is an in-progress CLI tool designed to simplify the process of building, deploying, and managing modern web applications.'
-  );
+  )
+  .version('0.0.0-dev.5', '-v, --version', 'Output the version number');
 
 program.addCommand(CreateApp);
 program.addCommand(Deploy);
+program.addCommand(UpdateScript);
 
 program.parse();
--- src/commands/update-scripts/update-scripts.ts
+++ src/commands/update-scripts/update-scripts.ts
@@ -1,7 +1,7 @@
 import { Command } from 'commander';
 import { createBashScripts } from '../create-app/fn/create-bash-scripts/create-bash-scripts.js';
 
-export const UpdateHost = new Command('update-scripts')
+export const UpdateScript = new Command('update-scripts')
   .alias('us')
   .description('Updates the bash scripts on the remote VPS.')
   .action(async () => {
