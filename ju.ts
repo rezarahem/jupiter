96d5894285cd0f2cc2898f86b1d70ee0d9239e1b
rahem <reza.rahem224@gmail.com>
2025-01-05 21:21:15 +0330

    pub dev.8

--- ju.ts
+++ ju.ts
@@ -10,7 +10,7 @@ program
   .description(
     'Jupiter is an in-progress CLI tool designed to simplify the process of building, deploying, and managing modern web applications.'
   )
-  .version('0.0.0-dev.7', '-v, --version', 'Output the version number');
+  .version('0.0.0-dev.8', '-v, --version', 'Output the version number');
 
 program.addCommand(InitApp);
 program.addCommand(Deploy);
