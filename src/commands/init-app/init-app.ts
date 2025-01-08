f21ed4ffc92cb0e29251de1462fb47829127f600
rahem <reza.rahem224@gmail.com>
2025-01-08 17:24:25 +0330

    update init

--- src/commands/init-app/init-app.ts
+++ src/commands/init-app/init-app.ts
@@ -3,7 +3,6 @@ import { gitIint } from './fn/git-init.js';
 import { createJupiterFile } from './fn/create-jupiter-file.js';
 import { newAppInputs } from './fn/get-user-input.js';
 import { addEnvVar } from '../../utils/add-env-var.js';
-import { getAppNameAndPorts } from './fn/get-app-name-and-ports.js';
 import { createDockerignore } from './fn/create-dockerignore.js';
 import { createDockerComposeBase } from './fn/create-docker-compose-base.js';
 import { addNextjs } from './fn/add-next/add-nextjs.js';
@@ -20,19 +19,11 @@ export const InitApp = new Command('initialize-app')
   .action(async ops => {
     const web = await checkWebApp();
 
-    switch (web) {
-      case 'nextjs':
-        await addNextjs();
-        break;
-      case 'nuxtjs':
-        // await addNuxtjs();
-        break;
-    }
-
     const { domain, email, repo, sshPort, vpsIp, vpsUsername } =
       await newAppInputs();
 
     gitIint();
+
     createJupiterFile();
 
     const currentDic = process.cwd();
@@ -61,16 +52,21 @@ export const InitApp = new Command('initialize-app')
       vpsUsername,
     });
 
+    switch (web) {
+      case 'nextjs':
+        await addNextjs();
+        break;
+      case 'nuxtjs':
+        // await addNuxtjs();
+        break;
+    }
     await createDockerignore(currentDic);
     await createDockerComposeBase(app as string);
-
-    if (app) {
-      await addEnvVar({
-        directory: currentDic,
-        filename: '.jupiter',
-        variables: {
-          APP: app,
-        },
-      });
-    }
+    await addEnvVar({
+      directory: currentDic,
+      filename: '.jupiter',
+      variables: {
+        APP: app as string,
+      },
+    });
   });
