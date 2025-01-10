270edc0a8dbb753d55de0a72cb018d6540a6a6ce
rahem <reza.rahem224@gmail.com>
2025-01-10 10:26:37 +0330

    update deploy cmd

--- src/commands/deploy/deploy.ts
+++ src/commands/deploy/deploy.ts
@@ -1,10 +1,6 @@
 import { Command } from 'commander';
-import { checkWebApp } from '../../utils/check-web-app.js';
 import { streamCommand } from '../../utils/stream-command.js';
 import dotenv from 'dotenv';
-import { hasServices } from './fn/has-service.js';
-import { cloneSource } from './fn/clone/clone-source.js';
-import { getFiles } from './fn/clone/get-files.js';
 
 const command = ({ variables }: { variables: Record<string, string> }) => {
   const envVars = Object.entries(variables)
@@ -17,55 +13,14 @@ export const Deploy = new Command('deploy')
   .alias('d')
   .description('Deploy the application to the specified environment')
   .action(async () => {
-    const WEB = await checkWebApp();
-
     dotenv.config({ path: '.jupiter' });
 
     const APP = process.env.APP;
-    const VPS_USERNAME = process.env.VPS_USERNAME;
-    const VPS_IP = process.env.VPS_IP;
-    const SSH_PORT = process.env.SSH_PORT;
-    const EMAIL = process.env.EMAIL;
-    const DOMAIN = process.env.DOMAIN;
-    const APOLLO = process.env.APOLLO;
-    const ARTEMIS = process.env.ARTEMIS;
-    const DOCKER_COMPOSE = hasServices() ? '1' : '0';
-    if (
-      !APP ||
-      !VPS_USERNAME ||
-      !VPS_IP ||
-      !SSH_PORT ||
-      !EMAIL ||
-      !DOMAIN ||
-      !APOLLO ||
-      !ARTEMIS ||
-      !WEB ||
-      !DOCKER_COMPOSE
-    ) {
-      console.error(
-        'Missing required environment variables: APP, EMAIL, or DOMAIN'
-      );
+
+    if (!APP) {
+      console.error('Missing required variable, App name');
       process.exit(1);
     }
 
-    const MANUAL = '1';
-
-    const cmd = command({
-      variables: {
-        APP,
-        EMAIL,
-        DOMAIN,
-        WEB,
-        APOLLO,
-        ARTEMIS,
-        DOCKER_COMPOSE,
-        MANUAL,
-      },
-    });
-
-    await cloneSource(APP);
-    await streamCommand(cmd);
-
-    // if (MANUAL === '1') {
-    // }
+    await streamCommand(`APP=${APP} ~/jupiter/jux/deploy.sh`);
   });
