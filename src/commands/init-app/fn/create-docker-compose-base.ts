b2cff30b984d01e396a9804003d43a2292bcb213
rahem <reza.rahem224@gmail.com>
2025-01-10 21:52:19 +0330

    update create docker compse base

--- src/commands/init-app/fn/create-docker-compose-base.ts
+++ src/commands/init-app/fn/create-docker-compose-base.ts
@@ -6,6 +6,7 @@ export const createDockerComposeBase = async (appName: string) => {
     networks: {
       [appName]: {
         name: appName,
+        external: true,
         driver: 'bridge',
       },
     },
